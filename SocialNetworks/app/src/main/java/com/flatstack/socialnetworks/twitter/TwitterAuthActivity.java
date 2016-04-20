0a46feafeb910dfa9eec1a94391754ff08b23324
Илья Еремин <partissanei@gmail.com>
2016-04-20 23:53:21 +0300

    move twitter to separate activity & update keys

---
+++ SocialNetworks/app/src/main/java/com/flatstack/socialnetworks/twitter/TwitterAuthActivity.java
@@ -0,0 +1,71 @@
+package com.flatstack.socialnetworks.twitter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.flatstack.socialnetworks.R;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+
+/**
+ * Created by Ilya Eremin on 1/6/16.
+ */
+public class TwitterAuthActivity extends AppCompatActivity {
+
+    private static final String KEY_FIRST_LAUNCH = "firstLaunch";
+
+    private boolean firstLaunch = true;
+
+    private TwitterAuthClient twitterAuthClient;
+
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_social_auth);
+        if (savedInstanceState != null) {
+            firstLaunch = savedInstanceState.getBoolean(KEY_FIRST_LAUNCH, true);
+        }
+        if (firstLaunch) {
+            // in real application create twitter client as singletone in dagger module
+            if (twitterAuthClient == null) {
+                twitterAuthClient = new TwitterAuthClient();
+            }
+            twitterAuthClient.authorize(this, new Callback<TwitterSession>() {
+                @Override
+                public void success(Result<TwitterSession> result) {
+                    authorizeWithTwitter(result.data.getAuthToken());
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+                    finish();
+                }
+            });
+            firstLaunch = false;
+        }
+    }
+
+    private void authorizeWithTwitter(TwitterAuthToken authToken) {
+        Toast.makeText(TwitterAuthActivity.this, "token is :" + authToken.token, Toast.LENGTH_SHORT)
+            .show();
+    }
+
+    @Override public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (twitterAuthClient == null) {
+            twitterAuthClient = new TwitterAuthClient();
+        }
+        twitterAuthClient.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_FIRST_LAUNCH, firstLaunch);
+    }
+}
--- SocialNetworks/app/src/main/java/com/flatstack/socialnetworks/ui/MainActivity.java
+++ SocialNetworks/app/src/main/java/com/flatstack/socialnetworks/ui/MainActivity.java
@@ -1,13 +1,10 @@
 package com.flatstack.socialnetworks.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
 import com.flatstack.socialnetworks.Navigator;
 import com.flatstack.socialnetworks.R;
-import com.flatstack.socialnetworks.authorization.TwitterLoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +16,4 @@ public class MainActivity extends AppCompatActivity {
         Navigator.mainScreen();
     }
 
-    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // if you use fragment you need add this line to make twitter authorization work!
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag(TwitterLoginFragment.TAG);
-        if (fragment != null) {
-            fragment.onActivityResult(requestCode, resultCode, data);
-        }
-    }
 }
