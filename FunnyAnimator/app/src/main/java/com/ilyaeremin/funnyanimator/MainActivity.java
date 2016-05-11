9319f1e2752c86ca9cc9ce599695a467d0bafdac
Илья Еремин <partissanei@gmail.com>
2016-05-11 10:24:29 +0300

    add view property screen

--- FunnyAnimator/app/src/main/java/com/ilyaeremin/funnyanimator/MainActivity.java
+++ FunnyAnimator/app/src/main/java/com/ilyaeremin/funnyanimator/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ilyaeremin.funnyanimator;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.rotate_and_jump).setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RotateAndJumpScreen.class));
+            }
+        });
+        findViewById(R.id.view_property_animator).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ViewPropertyAnimatorScreen.class));
             }
         });
     }
