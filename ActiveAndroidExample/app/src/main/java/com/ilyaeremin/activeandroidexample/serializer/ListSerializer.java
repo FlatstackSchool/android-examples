a26620e8ef63211e4cd41be3ab47eecb4cb6a34d
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2016-05-18 13:10:39 +0300

    remove info about String

--- ActiveAndroidExample/app/src/main/java/com/ilyaeremin/activeandroidexample/serializer/ListSerializer.java
+++ ActiveAndroidExample/app/src/main/java/com/ilyaeremin/activeandroidexample/serializer/ListSerializer.java
@@ -5,18 +5,14 @@ import android.support.annotation.Nullable;
 
 import com.activeandroid.serializer.TypeSerializer;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.ilyaeremin.activeandroidexample.Deps;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Ilya Eremin on 16.05.2016.
  */
 public class ListSerializer extends TypeSerializer {
-    public static final String TYPE_STRING = "String";
     private             Gson   mapper      = Deps.getInstance().getMapper();
 
     /**
@@ -37,38 +33,16 @@ public class ListSerializer extends TypeSerializer {
         if (data == null) {
             return null;
         }
-        List list = (List) data;
-        String objectName;
-        if (!list.isEmpty()) {
-            Object o = list.get(0);
-            if (o instanceof String) {
-                objectName = TYPE_STRING;
-            } else {
-                // or you can
-                // throw new IllegalStateException("unknown format");
-                return null;
-            }
-        } else {
-            return null;
-        }
-        return objectName + "\n" + mapper.toJson(data, List.class);
+
+        return mapper.toJson(data, List.class);
     }
 
     @Override @Nullable public List deserialize(@Nullable Object data) {
         if (data == null) {
             return null;
         }
-        String parsedString = (String) data;
-        String type = parsedString.substring(0, parsedString.indexOf("\n"));
-        String json = parsedString.substring(parsedString.indexOf("\n") + 1);
-        Type listType;
-        if (type.equals(TYPE_STRING)) {
-            listType = new TypeToken<ArrayList<String>>() {}.getType();
-        } else {
-//            throw new IllegalStateException("unknown list type");
-            return null;
-        }
-        return mapper.fromJson(json, listType);
+
+        return mapper.fromJson((String)data, List.class);
     }
 
 }
