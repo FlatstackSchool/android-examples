e7bd66b7b799f70f3d596357ccd98efff4849f07
Ilya Eremin <partissanei@gmail.com>
2017-05-21 22:48:05 +0300

    copy android base to android examples

---
+++ kotlin-base/app/src/main/java/com/flatstack/android/App.java
@@ -0,0 +1,12 @@
+package com.flatstack.android;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // your super code here
+    }
+}
